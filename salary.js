57cb9bdf89e4431e49abd73e1ab7acfd119066de
avadhkheni <avadhkheni29@gmail.com>
2024-11-27 10:15:24 +0530

    Add files via upload

---
+++ salary.js
@@ -0,0 +1,9 @@
+let salary = 160000;
+let HAR = 20;
+let DA = 5;
+let TA = 8;
+
+let totalSalary =
+  salary + (salary * HAR) / 100 + (salary * DA) / 100 + (salary * TA) / 100;
+
+console.log(totalSalary);
